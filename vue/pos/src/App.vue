39125e15cfa09270c4ff6b24c04b872b380b1924
thhelq <thug.lq@qq.com>
2019-08-14 15:45:47 +0800

    添加 POS 快餐店收银系统 demo

---
+++ vue/pos/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- 左侧导航 -->
+    <leftNav></leftNav>
+    <!-- 操作区域 -->
+    <div class="main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Microsoft YaHei', 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+.main {
+  float: left;
+  width: 95%;
+  height: 100%;
+  background-color: #EFF2F7;
+  overflow: auto;
+}
+</style>
+<script>
+import leftNav from '@/components/common/leftNav.vue'
+export default {
+  name: 'app',
+  components: {
+    leftNav
+  }
+}
+</script>
